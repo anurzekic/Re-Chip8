1c2fa163daf6aec8d6e85d3937fdaee519227102
Anur Zekic <anurzekic@gmail.com>
2025-09-18 21:47:08 +0200

    added imgui as submodule, updated cmake files

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include "imgui.h"
 #include "backends/imgui_impl_sdl3.h"
 #include "backends/imgui_impl_sdlrenderer3.h"
-#include "imgui_club/imgui_memory_editor.h"
+#include "imgui_memory_editor.h"
 
 #include <iostream>
 #include <filesystem>
