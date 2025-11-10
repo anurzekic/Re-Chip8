588b5e8f648c8a8d054e90ca691588db4a104c17
Anur Zekic <anurzekic@gmail.com>
2025-11-10 23:22:49 +0100

    removes unused render function

--- chip8/include/chip8/renderer.hpp
+++ chip8/include/chip8/renderer.hpp
@@ -10,9 +10,9 @@ public:
     ~Renderer() = default;
     Renderer(const Renderer&) = default;
 
-    void renderDisplay(const display_t& display);
     void updateTexture(const display_t& display, SDL_Texture* texture);
     void clearWindow();
+    
 private:
     SDL_Renderer *renderer;
     color background_color;
--- chip8/src/renderer.cpp
+++ chip8/src/renderer.cpp
@@ -14,28 +14,6 @@ Renderer::Renderer(SDL_Renderer& renderer) : renderer(&renderer) {
     draw_color.a = 100;
 }
 
-// TODO Either make one function or a function that calls either one of these methods depending if the current display is empty (clear)
-void Renderer::renderDisplay(const display_t& display) {
-    SDL_SetRenderDrawColor(renderer, background_color.r, background_color.g, background_color.b, background_color.a);
-    SDL_RenderClear(renderer);
-
-    SDL_SetRenderDrawColor(renderer, draw_color.r, draw_color.g, draw_color.b, draw_color.a);
-
-    for (int y = 0; y < WINDOW_HEIGHT; ++y) {
-        for (int x = 0; x < WINDOW_WIDTH; ++x) {
-            if (display[y][x]) {
-                SDL_FRect pixel{
-                    static_cast<float>(x) * SCALE,
-                    static_cast<float>(y) * SCALE,
-                    SCALE, SCALE
-                };
-                SDL_RenderFillRect(renderer, &pixel);
-            }
-        }
-    }
-    SDL_RenderPresent(renderer);
-}
-
 void Renderer::updateTexture(const display_t& display, SDL_Texture* texture) {
     uint32_t pixels[WINDOW_WIDTH * WINDOW_HEIGHT];
     int index = 0;
